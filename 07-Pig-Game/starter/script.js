fee8d01f76c6cfbac2f288c2a1c376eb4039cb09
pavanpai23 <pavanpai2006@gmail.com>
2025-11-30 23:05:48 +0530

    js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //selecting element
+const palyer0el=document.querySelector('.player--0');
+const palyer1el=document.querySelector('.player--1');
 const score0el = document.querySelector('#score--0');
 const score1el = document.getElementById('score--1'); //both are correct we can write both
 const current0e1=document.getElementById('current--0');
@@ -16,7 +18,17 @@ score0el.textContent = 0;
 score1el.textContent = 0;
 diceel.classList.add('hidden');
 
+const scores=[0,0];
 let currentscore=0;
+let activeplayer=0;
+
+const switchplayer=function(){
+    document.getElementById(`current--${activeplayer}`).textContent=0;
+    currentscore=0;             
+    activeplayer=activeplayer===0?1:0;
+    palyer0el.classList.toggle('player--active');
+    palyer1el.classList.toggle('player--active');
+}
 
 
 //rolling dice
@@ -25,7 +37,6 @@ btnRoll.addEventListener('click',function(){
 
     // 1.generating normal dice roll
 const dice = Math.trunc(Math.random() * 6) + 1;
-console.log(dice);
 
 
 // 2.diplay dice
@@ -33,8 +44,23 @@ diceel.classList.remove('hidden');
 diceel.src = `dice-${dice}.png`;
 
 // 3.check for rolled 1 if true then give dice to next player
-if(dice!=1){
+if(dice!==1){
     currentscore+=dice;
-    current0e1.textContent= currentscore//change later
+    document.getElementById(`current--${activeplayer}`).textContent=currentscore;
 }
+else{
+  switchplayer();
+}
+})
+
+btnHold.addEventListener('click',function(){
+        //add current score to active player score
+        scores[activeplayer]+=currentscore;
+        //score[1]=score[1]+currentscore;
+        document.getElementById(`score--${activeplayer}`).textContent=scores[activeplayer];
+
+
+
+        //switching to next player;
+        switchplayer();
 })
