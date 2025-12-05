2337b15628e07e9dd1edf2042e4dfd5d1b0d4582
pavanpai23 <pavanpai2006@gmail.com>
2025-12-05 23:14:58 +0530

    JS

---
+++ 08-Behind-the-Scenes/starter/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+function calcage(birthyear){
+    const age=2078-birthyear;
+
+    function  printage(){
+        const out=`${name},you are ${age},born in ${birthyear}`
+        console.log(out);
+    }
+    printage();
+    return age;
+}
+
+const name='jonas';
+calcage(2006);
+
