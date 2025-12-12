cb7b3fa4522edc1b91c12dc7a7c0832817b941d3
pavanpai23 <pavanpai2006@gmail.com>
2025-12-12 23:06:53 +0530

    JS

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,16 +1,26 @@
 'use strict';
 
+// // 
+// function calcage(birthyear){
+//     const age=2078-birthyear;
 
-function calcage(birthyear){
-    const age=2078-birthyear;
+//     function  printage(){
+//         const out=`${name},you are ${age},born in ${birthyear}`
+//         console.log(out);
+//     }
+//     printage();
+//     return age;
+// }
 
-    function  printage(){
-        const out=`${name},you are ${age},born in ${birthyear}`
-        console.log(out);
-    }
-    printage();
-    return age;
-}
+// const name='jonas';
+// calcage(2004);
 
-const name='jonas';
-calcage(2004);
+
+console.log(me);
+console.log(job);
+console.log(year);
+
+var me='pavan';
+let job='teacher';
+const year='2006';
+
