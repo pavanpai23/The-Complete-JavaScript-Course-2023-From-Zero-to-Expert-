534c9dbd8ffe65f1db5524b2224dd57bd69b9270
pavanpai23 <pavanpai2006@gmail.com>
2026-02-15 21:28:51 +0530

    JS

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -16,11 +16,61 @@
 // calcage(2004);
 
 
-console.log(me);
-console.log(job);
-console.log(year);
+// console.log(me);
+// console.log(job);
+// console.log(year);
 
-var me='pavan';
-let job='teacher';
-const year='2006';
+// var me='pavan';
+// let job='teacher';
+// const year='2006';
 
+// console.log(addDecl(2,3));
+// console.log(addexp(2,3));
+// // console.log(addarrow(2,3));
+
+// function addDecl(a,b){
+//     return a+b;
+// }
+
+// const addexp=function(a,b){
+//     return a+b;
+// }
+
+// var addarrow=(a,b)=>a+b;  
+
+/*
+  only function decoration works when we declare the console before only
+not works if we use const and var
+*/
+
+//this keyword
+
+// console.log(this);
+
+// const calcage=function(birthyear){
+//    console.log(2074-birthyear);
+//    console.log(this);   
+// }
+// calcage(2006); 
+
+
+// const calcageArrow=birthyear=>{
+//    console.log(2074-birthyear);
+//    console.log(this);   
+// }
+// calcageArrow(2016); 
+
+const jonas={
+    year:1991,
+    calcage:function(){
+        console.log(this);
+        console.log(2026-this.year);
+    }
+}
+
+jonas.calcage();
+
+const matlina={
+    year:2018,
+};
+matlina.calcage=jonas.calcage;
---
+++ 08-Behind-the-Scenes/starter/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+sole.log(this);
+
+// const calcage=function(birthyear){
+//    console.log(2074-birthyear);
+//    console.log(this);   
+// }
+// calcage(2006); 
