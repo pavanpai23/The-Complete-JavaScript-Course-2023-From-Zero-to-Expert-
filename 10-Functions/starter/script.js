ed98598fa0f7edb3812ebdfd7a2691f0f6d680d2
pavanpai23 <pavanpai2006@gmail.com>
2026-03-15 23:07:57 +0530

    JS

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -22,27 +22,122 @@
 // createBooking('NJ123',34,12);
 
 
-const flight='KMN233';
-const jhonas={
-    name:'jhonas schamas',
-    passport:231342234421,
-};
+// const flight='KMN233';
+// const jhonas={
+//     name:'jhonas schamas',
+//     passport:231342234421,
+// };
 
-const checkin=function(flightname,passenger){
-    flightname='KJARFS123';
-    passenger.name='Mr.' + passenger.name;
-    if(passenger.passport===231342234421){
-        alert('Checked in');
-    }else{
-        alert('Wrong passport');
+// const checkin=function(flightname,passenger){
+//     flightname='KJARFS123';
+//     passenger.name='Mr.' + passenger.name;
+//     if(passenger.passport===231342234421){
+//         alert('Checked in');
+//     }else{
+//         alert('Wrong passport');
+//     }
+// };
+// checkin(flight,jhonas);
+// console.log(flight);
+// console.log(jhonas);
+
+// const newpassport=function(person){
+//     person.passport=Math.trunc(Math.random*100000);
+// };
+// newpassport(jhonas);
+// checkin(flight,jhonas);
+
+
+// const greet=function(greeting){
+//     return function(name){
+//         console.log(`${greeting} ${name}`);
+//     };
+// };
+
+// const greethey=greet('hey');
+// greethey('jonas');
+// greethey('pavan');
+
+// greet('hello')('jhonsd'); 
+
+// const greetArr=greeting=>name=>console.log(`${greeting} ${name}`);
+
+// greetArr('hi')('heelo');
+
+// const lufthansa={
+//     airlines:'luftana',
+//     iatacode:'LH',
+//     booking:[],
+
+// book(flightnum  , name){
+//     console.log(`${name} booked a seat on ${this.airlines} flight ${this.iatacode} ${flightnum} `);
+// },
+// };
+
+// lufthansa.book('234','jhonas');
+// lufthansa.book('453','jhobsdf');
+
+// const eurowings={
+//     airline:'eurowings',
+//     iataCode:'EW',
+//     bookings:[],
+// };
+
+// const book=lufthansa.book;
+// //does not work
+// //book(23,'sarah williams);
+
+// book.call(eurowings,23,'sarah ');
+// console.log(eurowings);
+
+// book.call(lufthansa,239,'marry cooper')
+// console.log(lufthansa);
+
+
+// //bind methods
+// const bookew=book.bind(eurowings);
+// bookew(23,'neon'); 
+
+// //with event listener
+// lufthansa.planes=400;
+// lufthansa.buyPlane=function () {
+// console.log(this);
+// this.planes++;
+// console.log(this.planes);
+// }
+// document.querySelector('.buy').addEventListener('click',lufthansa.buyPlane.bind(lufthansa));
+
+
+//Question 1
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+
+  registerNewAnswer(){
+    const answer=Number(prompt(`${this.question}\n${this.options.join('\n')} \n('Write option num)`),
+);
+console.log(answer);
+
+//register answer
+typeof answer==='number'&& answer<this.answers.length && this.answers[answer]++;
+
+    this.displayresults();
+    this.displayresults('String');
+  },
+
+displayresults(type='array'){
+    if(type==='array'){
+        console.log(this.answers);
+    }else if(type==='string'){
+        console.log(`poll result are ${this.answers.join(',')}`);
     }
+    },
 };
-checkin(flight,jhonas);
-console.log(flight);
-console.log(jhonas);
 
-const newpassport=function(person){
-    person.passport=Math.trunc(Math.random*100000);
-};
-newpassport(jhonas);
-checkin(flight,jhonas);
+// poll.registerNewAnswer();
+
+document.querySelector('.poll').addEventListener('click',poll.registerNewAnswer.bind(poll));
+
+poll.displayresults.call({answer:[5,3,4]},'string');
